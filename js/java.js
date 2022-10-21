9dec8176f933413972436182e62c07c8492177de
mostafa mahmoud <74399214+mostafa2742002@users.noreply.github.com>
2022-10-21 20:51:09 +0200

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,22 @@
+function clicklinkedin(){
+    window.open("https://www.linkedin.com/in/mostafa-mahmoud-927164202/", "_blank");
+}
+function clickgithub(){
+    window.open("https://github.com/mostafa2742002", "_blank");
+}
+function clicktelegram(){
+    window.open("https://t.me/sasa274", "_blank");
+}
+function clickemail(){
+    window.open("mailto:mostafa19500mahmoud@gmail.com", "_blank");
+}
+function clickfacebook(){
+    window.open("https://www.facebook.com/profile.php?id=100007991656437", "_blank");
+}
+function web(){
+    window.location.href = "front.html";
+}
+function back_end(){
+    window.location.href = 'back_end.html';
+}
+
